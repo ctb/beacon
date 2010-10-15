bc45c96b3b397336a72c302b77a60656e31b8c89
C. Titus Brown <titus@idyll.org>
2010-10-15 16:07:24 +0000

    fix path

--- ltee/run-forever.sh
+++ ltee/run-forever.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
+
+# run things for the first time.
+cp -r run.template run.1
+cd run.1
+avida
+cd ..
+
+# now, run forever and use CTRL-C to exit.  if you want to do only a
+# limited set of serial transfers, then use a 'for' loop instead; see
+# http://tldp.org/LDP/Bash-Beginners-Guide/html/chap_09.html
 while true;
 do
-   python ~/beacon/ltee/run-next.py . || break
+   python /mnt/beacon/ltee/run-next.py . || break
 done
